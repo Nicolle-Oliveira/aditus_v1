8d330cc639be31f9222376141d3fb06555a00b25
Vitoria <104925527+vitoriaEHon@users.noreply.github.com>
2023-11-13 14:44:53 -0300

    Update perfil_page.dart

--- lib/pages/perfil_page.dart
+++ lib/pages/perfil_page.dart
@@ -1,14 +1,64 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../services/user_info_service.dart';
 
-class PerfilPage extends StatelessWidget {
-  const PerfilPage({super.key});
+class PerfilPage extends StatefulWidget {
+  const PerfilPage({Key? key}) : super(key: key);
 
-/*--------------------------NÃO MEXER--------------------------*/
+  @override
+  _PerfilPageState createState() => _PerfilPageState();
+}
+
+class _PerfilPageState extends State<PerfilPage> {
+  bool isEditing = false;
+  String biografia =
+      "Escreva sobrre você!";
+
+  void updateBiografia(String newBiografia) {
+    setState(() {
+      biografia = newBiografia;
+    });
+  }
+
+  Widget buildRichText(String text) {
+    final List<InlineSpan> children = [];
+    final List<String> parts = text.split(":");
+    if (parts.length == 2) {
+      children.add(
+        TextSpan(
+          text: parts[0] + ": ",
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF000A4D),
+          ),
+        ),
+      );
+      children.add(
+        TextSpan(
+          text: parts[1],
+          style: TextStyle(
+            fontSize: 18,
+            color: Colors.black,
+          ),
+        ),
+      );
+    } else {
+      children.add(
+        TextSpan(
+          text: text,
+          style: TextStyle(
+            fontSize: 18,
+            color: Color.fromARGB(255, 0, 0, 0),
+          ),
+        ),
+      );
+    }
+    return RichText(text: TextSpan(children: children));
+  }
+
   @override
   Widget build(BuildContext context) {
     final userInfo = Provider.of<UserInfo>(context, listen: false);
@@ -17,16 +67,12 @@ class PerfilPage extends StatelessWidget {
       future: userInfo.getCurrentUserDocument(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          // Exibe um indicador de carregamento enquanto a Future não foi concluída.
           return Center(child: CircularProgressIndicator());
         } else if (snapshot.hasError) {
-          // Lida com erros, se houver.
           return Center(child: Text('Erro: ${snapshot.error}'));
         } else if (!snapshot.hasData || snapshot.data == null) {
-          // Lida com o caso em que o userDocument é nulo.
           return Center(child: Text('Nenhum documento encontrado'));
         } else {
-          // Acessa os dados do documento após a conclusão da Future.
           final userDocument = snapshot.data!;
           final data = userDocument.data() as Map<String, dynamic>?;
 
@@ -35,29 +81,207 @@ class PerfilPage extends StatelessWidget {
             String sobrenome = data['sobrenome'] ?? 'Sobrenome não encontrado';
             String email = data['email'] ?? 'Email não encontrado';
             bool isVolunteer = data['isVoluntario'] ?? false;
-/*--------------------------FIM DA ÁREA NÃO MEXER--------------------------*/
-        
-            //Aqui no scaffold pode :3
+
             return Scaffold(
-              appBar: AppBar(
-                backgroundColor: Color.fromARGB(255, 205, 207, 240),
-                title: Text(
-                  'Perfil',
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 67, 69, 110),
+              body: Container(
+                color: Colors.white,
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        color: Color(0xFFCDCFF0),
+                        padding: EdgeInsets.all(16),
+                        width: double.infinity,
+                        child: Column(
+                          children: [
+                            Stack(
+                              children: [
+                                Container(
+                                  width: 130,
+                                  height: 130,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                        width: 4, color: Color(0xFFCDCFF0)),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        spreadRadius: 2,
+                                        blurRadius: 10,
+                                        color: Colors.black.withOpacity(0.1),
+                                      ),
+                                    ],
+                                    shape: BoxShape.circle,
+                                  ),
+                                  child: Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        color:
+                                            Color.fromARGB(255, 177, 179, 216),
+                                        shape: BoxShape.circle,
+                                        image: DecorationImage(
+                                          fit: BoxFit.cover,
+                                          image: NetworkImage(
+                                            'https://akamai.sscdn.co/uploadfile/letras/fotos/a/c/5/c/ac5cf7760bf3453f014dbff76247be8b.jpg',
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Positioned(
+                                  bottom: 0,
+                                  right: 0,
+                                  child: Container(
+                                    height: 40,
+                                    width: 40,
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      border: Border.all(
+                                          width: 4, color: Colors.white),
+                                      color: Color(0xFF071992),
+                                    ),
+                                    child: Icon(
+                                      Icons.edit,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Container(
+                        padding: EdgeInsets.all(16),
+                        width: double.infinity,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Informações',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                                color: Color(0xFF000A4D),
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Padding(
+                              padding: EdgeInsets.only(left: 2),
+                              child: buildRichText("Nome: $nome"),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 2),
+                              child: buildRichText("Sobrenome: $sobrenome"),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 2),
+                              child: buildRichText("Email: $email"),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 2),
+                              child: buildRichText(
+                                  'É Voluntário: ${isVolunteer ? 'Sim' : 'Não'}'),
+                            ),
+                            SizedBox(height: 5),
+                            Text(
+                              'Biografia',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                                color: Color(0xFF000A4D),
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            if (isEditing)
+                              TextFormField(
+                                decoration: InputDecoration(
+                                  labelText: "Biografia:",
+                                  hintText: "Digite sua biografia",
+                                ),
+                                initialValue: biografia,
+                                onChanged: (value) {
+                                  updateBiografia(value);
+                                },
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Color.fromARGB(255, 0, 0, 0),
+                                ),
+                              ),
+                            if (!isEditing)
+                              Padding(
+                                padding: EdgeInsets.only(left: 2),
+                                child: buildRichText(biografia),
+                              ),
+                            SizedBox(height: 5),
+                            if (!isEditing)
+                              ElevatedButton(
+                                onPressed: () {
+                                  setState(() {
+                                    isEditing = true;
+                                  });
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  padding: EdgeInsets.symmetric(horizontal: 10),
+                                  shape: CircleBorder(),
+                                  primary: Colors.white,
+                                ),
+                                child: Icon(
+                                  Icons.edit,
+                                  color: Color(0xFF34408E),
+                                ),
+                              ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                if (isEditing)
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        isEditing =
+                                            false; // Sair do modo de edição
+                                      });
+                                    },
+                                    child: Text("CANCELAR"),
+                                    style: ElevatedButton.styleFrom(
+                                      primary: Colors.red,
+                                      padding:
+                                          EdgeInsets.symmetric(horizontal: 10),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(20),
+                                      ),
+                                    ),
+                                  ),
+                                if (isEditing)
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        isEditing =
+                                            false; // Sair do modo de edição ao salvar
+                                      });
+                                      // Aqui você pode salvar a biografia, se necessário
+                                    },
+                                    child: Text("SALVAR"),
+                                    style: ElevatedButton.styleFrom(
+                                      primary: Color(0xFF5960CD),
+                                      padding:
+                                          EdgeInsets.symmetric(horizontal: 10),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(20),
+                                      ),
+                                    ),
+                                  ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-                centerTitle: true,
-              ),
-              body: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text('Nome: $nome'),
-                    Text('Sobrenome: $sobrenome'),
-                    Text('Email: $email'),
-                    Text('É Voluntário: ${isVolunteer ? 'Sim' : 'Não'}'),
-                  ],
                 ),
               ),
             );
@@ -69,69 +293,3 @@ class PerfilPage extends StatelessWidget {
     );
   }
 }
-/*
-import 'package:flutter/material.dart';
-
-class PerfilPage extends StatefulWidget {
-  const PerfilPage({super.key});
-
-  @override
-  State<PerfilPage> createState() => _PerfilPageState();
-}
-
-class _PerfilPageState extends State<PerfilPage> {
-  bool isObscurePassword = true;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xFFCDCFF0),
-        title: Text(
-          'Perfil',
-          style: TextStyle(
-            color: Color.fromARGB(255, 67, 69, 110),
-          ),
-        ),
-        centerTitle: true,
-      ),
-      body: SafeArea(
-          child: SingleChildScrollView(
-              child: Center(
-        child: Column(children: [Text('constroi depois sô')]),
-      ))),
-
-    );
-  }
-
-  Widget buildTextField(
-    String labelText,
-    String placeholder,
-    bool isPasswordTextField,
-  ) {
-    return Padding(
-      padding: EdgeInsets.only(bottom: 30),
-      child: TextField(
-        obscureText: isPasswordTextField ? isObscurePassword : false,
-        decoration: InputDecoration(
-          suffixIcon: isPasswordTextField
-              ? IconButton(
-                  icon: Icon(Icons.remove_red_eye,
-                      color: Color.fromARGB(255, 106, 108, 151)),
-                  onPressed: () {},
-                )
-              : null,
-          contentPadding: EdgeInsets.only(bottom: 5),
-          labelText: labelText,
-          floatingLabelBehavior: FloatingLabelBehavior.always,
-          hintText: placeholder,
-          hintStyle: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-            color: const Color.fromARGB(255, 33, 31, 31),
-          ),
-        ),
-      ),
-    );
-  }
-}
-*/
